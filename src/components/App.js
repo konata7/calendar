6e8fb0d6f79af06fc29aa6c67c02a6aadbd264d9
JamieFerris <nps777@live.ru>
2022-07-06 19:33:53 +0300

    fix: ran npm audit; fixed vulnerability issues (updated packages)

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import styled from 'styled-components'
 import moment from 'moment'
 import {HeaderBar} from "./HeaderBar";
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import {styles} from './index.css'
+import './index.css'
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
