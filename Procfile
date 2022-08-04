881c611481142d01e779ab014ddd1aa6a223beba
JamieFerris <nps777@live.ru>
2022-08-04 16:05:56 -0700

    fix: Procfile for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: npm run start & npm run serve & wait -n;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css'
-import App from './components/App';
+import App from './components/App/index';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
